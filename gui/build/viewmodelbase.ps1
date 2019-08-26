42439b46ca009e3031388eb972832d6acd2922d5
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-08-25 23:32:04 -0400

    remove debug

--- gui/build/viewmodelbase.ps1
+++ gui/build/viewmodelbase.ps1
@@ -3,4 +3,5 @@ Import-Module ".\gui\PSAvalonia\1.0\PSAvalonia.psd1"
 class viewmodelbase : ReactiveUI.ReactiveObject{}
 
 class MainWindowViewModel : viewmodelbase {
+    [String]$Greeting = "Hello World!"
 }
