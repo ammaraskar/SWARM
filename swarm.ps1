f8be55f97c668addf165ef545f37229ce26104c9
maynardminer <maynardviii@gmail.com>
2020-01-15 13:40:38 -0500

    small fixes

--- swarm.ps1
+++ swarm.ps1
@@ -3,13 +3,22 @@ Using module ".\core\swarm.psm1";
 
 ## Environment
 $Dir = [IO.Path]::GetDirectoryName($script:MyInvocation.MyCommand.Path)
-if($IsWindows) { $Scripts = [IO.Path]::Join($Dir,"win"); }
-if($IsLinux) { $Scripts = [IO.Path]::Join($Dir,"linux"); }
+if ($IsWindows) { $Scripts = [IO.Path]::Join($Dir, "win"); }
+if ($IsLinux) { $Scripts = [IO.Path]::Join($Dir, "linux"); }
 $Target1 = [EnvironmentVariableTarget]::Machine
 $Target2 = [EnvironmentVariableTarget]::Process
-$PATH = ([Environment]::GetEnvironmentVariable('PATH',$Target1)).Split(";") | Where {$_ -ne "" } | Where { $_ -notlike "*SWARM*" }
-if($Scripts -NotIn $PATH) { $PATH += $Scripts; }
-$PATH = $PATH -Join ";";
+
+if ($IsLinux) {
+    $PATH = ([Environment]::GetEnvironmentVariable('PATH')).Split(":") | Where { $_ -ne "" } | Where { $_ -notlike "*SWARM*" }
+    if ($Scripts -NotIn $PATH) { $PATH += $Scripts; }
+    $PATH = $PATH -Join ":"
+}
+elseif ($IsWindows) {
+    $PATH = ([Environment]::GetEnvironmentVariable('PATH', $Target1)).Split(";") | Where { $_ -ne "" } | Where { $_ -notlike "*SWARM*" }
+    if ($Scripts -NotIn $PATH) { $PATH += $Scripts; }
+    $PATH = $PATH -Join ";";
+
+}
 
 ## Set for both machine and this process.
 [environment]::SetEnvironmentVariable('SWARM_DIR', $Dir, $Target1);
