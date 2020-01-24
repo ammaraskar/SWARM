9ae4fe71485aefabcf9abc4813996030038f143a
maynardminer <maynardviii@gmail.com>
2020-01-23 21:40:45 -0500

    v2 update

--- gui/gui_start.ps1
+++ gui/gui_start.ps1
@@ -2,7 +2,6 @@
 $libs = Get-ChildItem ".\gui\libs"
 foreach($file in $libs) {
     if($file.extension -eq ".dll" -and $file.BaseName -ne "libSkiaSharp" -and $File.BaseName -ne "Serilog") {
-        Write-Host "Adding $($file.name)"
         Add-Type -Path $file.Fullname
     }
 }
