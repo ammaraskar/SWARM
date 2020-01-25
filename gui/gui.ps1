1bf1f25f4d9c1772e301a6505230670f3bbce97e
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-01-25 05:02:41 -0500

    update GUI v2

--- gui/gui.ps1
+++ gui/gui.ps1
@@ -3,8 +3,11 @@ using namespace Avalonia.Controls;
 using namespace PSAvalonia;
 using module ".\ViewModels\MainWindow.xaml.psm1"
 
+
+
 $xaml = Get-Content ".\gui\ViewModels\MainWindow.xaml" | Out-String
-$Test = [GUI]::CreateWindow($xaml)
-$MainWindow = [MainWindow]::New()
-$Test.DataContext = $MainWindow
-[GUI]::ShowWindow($Test);
+$Global:MainWindow = [GUI]::CreateWindow($xaml)
+$MainWindow_Context = [MainWindow]::New()
+$MainWindow.HasSystemDecorations = $false;
+$MainWindow.DataContext = $MainWindow_Context
+[GUI]::ShowWindow($MainWindow);
