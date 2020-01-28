e657d0f9578d7028dcd16d6e45699baa621da689
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-01-27 19:51:52 -0500

    small tweaks

--- core/helper.psm1
+++ core/helper.psm1
@@ -4,6 +4,35 @@ class filedata {
     static [hashtable] stringdata([string[]]$file) {
         $table = @{ }
         [string[]]$Data = [IO.File]::ReadAllLines($File);
+
+        ## CUSTOM_USER_CONFIG can be multiline. Extract it.
+        $CUSTOM_USER_CONFIG = $null
+        if ($Data -like "*CUSTOM_USER_CONFIG=`'{*") {
+
+            ## Extract json data from CUSTOM USER CONFIG and its position
+            $Start = $Data.IndexOf("CUSTOM_USER_CONFIG=`'{")
+            $Cut = $Data[$Start .. $Data.Count]
+            $End = $Cut.IndexOf("}`'")
+            $Get = $Cut[0 .. $End]
+            $End = $Start + $End
+            $CUSTOM_USER_CONFIG = $Get.Replace("CUSTOM_USER_CONFIG=`'", "")
+            $CUSTOM_USER_CONFIG = $CUSTOM_USER_CONFIG.Replace("}`'", "}")
+            $CUSTOM_USER_CONFIG = $CUSTOM_USER_CONFIG | ConvertFrom-Json
+
+            ## Rebuild Array without custom user config- It will be added in later.
+            [string[]]$New = @()
+            for ($i = 0; $i -lt $Data.Count; $i++) {
+                if ($i -lt $start) {
+                    $New += $Data[$i]
+                }
+                elseif ($i -gt $Start -and $i -gt $end) {
+                    $New += $Data[$i]
+                }
+            }
+            $Data = $New
+        }
+
+
         $Where = [func[string, bool]] { $args -like "*=*" };
         $Lines = [Linq.Enumerable]::Where($Data, $Where) -replace "`"", ""
         $Lines | Foreach { 
@@ -12,12 +41,24 @@ class filedata {
             $Value = [Linq.Enumerable]::Last($split);
             $table.Add($Name, $Value);
         }
+
+        if ($CUSTOM_USER_CONFIG) {
+            $table.ADD("CUSTOM_USER_CONFIG", $CUSTOM_USER_CONFIG);
+        }
+
         return $table
     }
 
-    static [void] to_file([string]$filename,[hashtable]$data) {
+    static [void] to_file([string]$filename, [hashtable]$data) {
         $file_data = @()
-        $data.keys | ForEach-Object { $file_data += "$($_)=$($data.$_)" }
+        $data.keys | ForEach-Object { 
+            if ($Data.$_ -is [Hashtable] -or $Data.$_ -is [PSCustomObject]) {
+                $file_data += "$($_)=`'$($data.$_ | ConvertTo-Json)`'"
+            }
+            else {
+                $file_data += "$($_)=$($data.$_)" 
+            }
+        }
         [IO.File]::WriteAllLines($filename, $file_data)
     }
 }
