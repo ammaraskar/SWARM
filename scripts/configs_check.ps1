5c45768eb1f96a14e2b09df89b0ef7d44dd3f032
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-01-27 20:57:22 -0500

    v2 tweaks

--- scripts/configs_check.ps1
+++ scripts/configs_check.ps1
@@ -13,6 +13,7 @@ $Args += "swarm"
 Set-Location $env:SWARM_DIR
 
 $HIVEOS_CONF = Join-Path $env:SWARM_DIR "configs\web\hiveos_rig.conf"
+$H_MANIFEST = Join-Path $env:SWARM_DIR "configs\web\h-manifest.conf"
 $DEFAULTS = Join-Path $env:SWARM_DIR "configs\paramters\hiveos_rig.conf"
 
 $CONFIGS = @{ }
@@ -21,18 +22,38 @@ if(test-path($HIVEOS_CONF)){
     $CONFIGS.Add("hiveos_rig",[FileData]::StringData($HIVEOS_CONF));
 }
 
+if(test-path($H_MANIFEST)){
+    $CONFIGS.Add("h_manifest",[FileData]::StringData($H_MANIFEST));
+}
+
 
 
 ## handle args
 if($args[0] -eq "list") {
+    Write-Host "${Global:BYellow}##### HIVEOS RIG.CONF #####${Global:NoColor}"
     Foreach ($key in $CONFIGS.hiveos_rig.keys) {
         $items = $Configs.hiveos_rig
-        Write-Host "${Global:Yellow}$($key):${Global:NoColor}"
+        Write-Host "${Global:BYellow}$($key):${Global:NoColor}"
         foreach($value in $items.$key) {
             Write-Host "  $value"
         }
         Write-Host ""
     }
+    Write-Host "${Global:BYellow}##########${Global:NoColor}"
+    Write-Host ""
+
+    Write-Host "${Global:YELLOW}##### SWARM H-MANIFEST #####${Global:NoColor}"
+    Foreach ($key in $CONFIGS.h_manifest.keys) {
+        $items = $Configs.h_manifest
+        Write-Host "${Global:YELLOW}$($key):${Global:NoColor}"
+        foreach($value in $items.$key) {
+            Write-Host "  $value"
+        }
+        Write-Host ""
+    }
+    Write-Host "${Global:YELLOW}##########${Global:NoColor}"
+    Write-Host ""
+
 }
 elseif($args[0] -eq "json") {
    return $CONFIGS | ConvertTo-Json -Depth 5 -Compress
--- win/configs_check.bat
+++ win/configs_check.bat
@@ -1,4 +1,4 @@
 @echo off
 cd %SWARM_DIR%
 
-pwsh -command ".\scripts\gpu_check.ps1 %*"
+pwsh -command ".\scripts\configs_check.ps1 %*"
