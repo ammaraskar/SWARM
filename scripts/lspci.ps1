41fbae659a5cbf107882d6d1d32d767e6bc0a771
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-02-05 04:23:46 -0500

    pciutils for powershell

--- scripts/lspci.ps1
+++ scripts/lspci.ps1
@@ -1,3 +1,20 @@
+<#
+PCIUtils FOR POWERSHELL
+This is open-source software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+SWARM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+If you use my code, please include my github: https://github.com/maynardminer
+#>
+
+
 $Devices = Get-CimInstance -class Win32_PnPEntity | Where { $_.PNPDeviceID -match "PCI\\*" } | Select -Unique
 $pci = Get-Content ".\apps\device\pci_ids.json" | ConvertFrom-Json
 $alphabet = @()  
