ce0f1147d8497d62f9620fba38b24756935b0bed
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-02-06 19:10:04 -0500

    lscpi v2 update (test)   -Get-CIMInstance powershell cmdlet is failing    to get all PCI devices or is missing information.   -This version will use registry values instead.

--- scripts/lspci.ps1
+++ scripts/lspci.ps1
@@ -12,38 +12,70 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 If you use my code, please include my github: https://github.com/maynardminer
+
+If you find it useful - Consider a BTC donation: 1FpuMha1QPaWS4PTPZpU1zGRzKMevnDpwg
 #>
+
+Using namespace Microsoft.Win32;
+
+## A single device
+class Device {
+    [string]$HardwareID
+    [string]$CompatIds
+    [string]$iRev
+    [string]$iTitle
+    [string]$iVendor
+    [string]$iDevice
+    [string]$iDeviceSubsys
+    [string]$imanufacturer
+    [string]$iLocation
+
+    Device([string]$id, [string]$loc, [string]$compat) {
+        $this.HardwareID = $id;
+        $this.CompatIds = $compat;
+        $location_map = $loc.split(";")[2].TrimStart("(").TrimEnd(")").split(",");
+        [int]$get_busid = $location_map[0];
+        [int]$get_deviceID = $location_map[1]
+        [int]$get_functionId = $location_map[2]
+        $new_busid = "{0:x2}" -f $get_busid
+        $new_deviceID = "{0:x2}" -f $get_deviceID
+        $new_functionId = "{0:x2}" -f $get_functionId
+        $this.iLocation = "$new_busid`:$new_deviceID`:$new_functionId"
+    }
+}
 
 
 ## USE CIM to get to PnP Devices- To my knowledge this should not require administrator,
 ## but you know...Windows...
-$Devices = Get-CimInstance -class Win32_PnPEntity | Where { $_.PNPDeviceID -match "PCI\\*" } | Select -Unique
+## Scratch that- Get-CimInstace Win32_PnPEntity is garbage. I found it missing PCI devices.
 
+#$Devices = Get-CimInstance -class Win32_PnPEntity | Where { $_.PNPDeviceID -match "PCI\\*" } | Select -Unique
+
+## Use registry entries instead
+$Devices = @()
+$PCI_List = Get-ChildItem "HKLM:\SYSTEM\CurrentControlSet\Enum\PCI"
+$Registry = [RegistryKey]::OpenBaseKey([RegistryHive]::LocalMachine, [RegistryView]::Default).OpenSubKey("SYSTEM\CurrentControlSet\Enum\PCI")
+$PCI_List = $Registry.GetSubKeyNames()
+foreach($item in $PCI_List) {
+    $pci_key = $Registry.OpenSubKey($item)
+    $sub_names = $pci_key.GetSubKeyNames()
+    foreach($sub_value in $sub_names) {
+        $Sub_key = $pci_key.OpenSubKey($sub_value)
+        $names = $Sub_key.GetValueNames()
+        if(
+            "HardwareID" -in $names -and
+            "LocationInformation" -in $names -and
+            "CompatibleIDs" -in $names
+        ) { 
+            $HID = $sub_key.GetValue("HardwareID") | Where {$_ -like "*REV*"} | Select -First 1;
+            $LOC = $sub_key.GetValue("LocationInformation") | Select -First 1
+            $COM = $sub_key.GetValue("CompatibleIDs") | Where {$_ -like "*CC*"} | Select -First 1;
+            $Devices += [Device]::New($HID,$LOC,$COM)
+        }
+    }   
+}
 ## This is a json list of pci.ids.
 $pci = Get-Content ".\apps\device\pci_ids.json" | ConvertFrom-Json
-
-## First we need to get locations
-## I have found unlike linux that Windows
-## can have devices listed without locations
-## We only want devices with locations.
-foreach ($Device in $Devices) {
-    $device_Id = $Device.PNPDeviceID
-    $locations = ((get-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Enum\$device_ID" -name locationinformation).locationINformation).split(";")[2]
-    if ($locations) {
-        $location_map = $locations.TrimStart("(").TrimEnd(")")
-        $location = $location_map.split(",")
-
-        [int]$get_busid = $location[0]
-        [int]$get_deviceID = $location[1]
-        [int]$get_functionId = $location[2]
-
-        $new_busid = "{0:x2}" -f $get_busid
-        $new_deviceID = "{0:x2}" -f $get_deviceID
-        $new_functionId = "{0:x2}" -f $get_functionId
-
-        $Device | Add-Member "ilocation" "$new_busid`:$new_deviceID`:$new_functionId"
-    }
-}
 
 ## IF using parsable argument to get a single device
 if ($args[0] -eq "-vmms") {
@@ -57,30 +89,11 @@ else {
 ## but so far I haven't found a device yet
 ## that doesn't match pci.ids
 foreach ($Device in $Devices) {
-    $id = $null
-    $Value = $null
-    $vendorId = $null
-    $deviceId = $null
-    $deviceSubsysId = $null
-    $manufacturerId = $null
-    $vendor = $null
-    $device_name = $null
-    $manufacturer = $null
-    $cc = $null
-    $code = $null
-    $Code_Id = $null
-    $title = $null
-    $rev = $null
-    $revision = $null
-    $new_rev = $null
-    $deviceSubsys = $null
-
-    $id = $Device.DeviceID
-    $Value = $id.split('&')
-    $vendorId = $value[0].Substring($Value[0].Length - 4)
-    $deviceId = $value[1].Substring($Value[1].Length - 4)
-    $deviceSubsysId = $value[2].Substring($Value[2].Length - 4) + ' ' + $value[2].Substring($Value[2].Length - 8, 4)
-    $manufacturerId = $value[2].Substring($Value[2].Length - 4)
+    $IDs = $Device.HardwareID.Split('&')
+    $vendorId = $IDs[0].Substring($IDs[0].Length - 4)
+    $deviceId = $IDs[1].Substring($IDs[1].Length - 4)
+    $deviceSubsysId = $IDs[2].Substring($IDs[2].Length - 4) + ' ' + $IDs[2].Substring($IDs[2].Length - 8, 4)
+    $manufacturerId = $IDs[2].Substring($IDs[2].Length - 4)
  
     $vendor = $pci.PSobject.Properties.name | Where { $_.substring(0, 4) -eq $vendorId }
     $device_name = $pci.$vendor.PSObject.Properties.Name | Where { $_.substring(0, 4) -eq $deviceId }
@@ -88,8 +101,7 @@ foreach ($Device in $Devices) {
     if ($null -eq $deviceSubsys) { $deviceSubsys = ($device_name.split("   ")[1]) }
     $manufacturer = $pci.PSobject.Properties.name | Where { $_.substring(0, 4) -eq $manufacturerId }
 
-    $CC = $device.CompatibleID | Where { $_ -like "*CC_*" } | Select -First 1
-    $Get = $CC.IndexOf("CC_")
+    $CC = $device.CompatIds | Where { $_ -like "*CC_*" } | Select -First 1
     $CC = $CC.Substring(13 + 3, 4)
 
     $Code = $CC.Substring(0, 2)
@@ -99,16 +111,16 @@ foreach ($Device in $Devices) {
         $title = $pci.$title.PSObject.Properties.Name | Where { $_.substring(0, 2) -eq $Code_Id }
     }
 
-    $rev = $Device.DeviceID.IndexOf("REV_")
-    $revision = $Device.DeviceID.substring($rev + 4, 2)
+    $rev = $Device.HardwareID.IndexOf("REV_")
+    $revision = $Device.HardwareID.substring($rev + 4, 2)
     $new_rev = "{0:x2}" -f $revision
 
-    $Device | Add-Member "irev" $new_rev
-    $Device | Add-Member "ititle" ($title.split("   ")[1])
-    $Device | Add-Member "ivendor" ($vendor.split("   ")[1])
-    $Device | Add-Member "idevice" ($device_name.split("   ")[1])
-    $Device | Add-Member "idevicesubsys" $deviceSubsys
-    $Device | Add-Member "imanufacturer" ($manufacturer.split("   ")[1])
+    $Device.Irev = $new_rev
+    $Device.iTitle = ($title.split("   ")[1])
+    $Device.iVendor = ($vendor.split("   ")[1])
+    $Device.iDevice = ($device_name.split("   ")[1])
+    $Device.Idevicesubsys = $deviceSubsys
+    $Device.iManufacturer = ($manufacturer.split("   ")[1])
 }
 
 ## Print single view
